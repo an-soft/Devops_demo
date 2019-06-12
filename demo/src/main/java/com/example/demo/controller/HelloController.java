c5f4d405216b1f59fc5fa32eb9c3c5fb2f4c8cc4
yadavviju <49593782+yadavviju@users.noreply.github.com>
2019-06-11 22:43:41 -0400

    Update HelloController.java

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.lang.*;
 
 @RestController
 public class HelloController {
@@ -10,12 +9,7 @@ public class HelloController {
     @RequestMapping("/hello")
     public String getHelloMessage() {
         //injecting sonar issue
-        try {
-    System.out.print("hello");
-  }
-  catch (NullPointerException e) {
-   System.out.print(e);
-  }
+        // System.out.print("hello");
         return "Hello World Demo";
     }
 }
