fb94f311d426a5e5b6e071f34e0a14ec6e629864
yadavviju <49593782+yadavviju@users.noreply.github.com>
2019-06-18 19:19:09 -0400

    Update DemoApplicationTests.java

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -9,11 +9,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
 public class DemoApplicationTests {
-      String message = "Hello World";	
-    
-    @Test
-    void whenCallingSayHello_thenReturnHello() {
-        assertTrue("Hello World".equals(message));
-    }
+     	
+   String message = "Hello World";	
+
+   @Test
+   public void testPrintMessage() {
+      assertEquals(message,"Hello World");
+   }
 
 }
